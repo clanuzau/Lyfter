b2ba9c8256c46373fddb723d9c95c835930342df
Cesar Lanuza <Cesar.Lanuza@hotmail.com>
2026-06-17 13:30:39 -0600

    feat: Add random number guessing game

--- Lyfter-Labs/phyton06.py
+++ Lyfter-Labs/phyton06.py
@@ -1,11 +1,11 @@
 """
-Ejercicio # 6 - Python Basic - Adivinar numero aleatorio entre 1 y 10
+Ejercicio # 6 - Python Basic - Adivinar numero secreto entre 1 y 10
 Student: Cesar Lanuza Urbina
 """
 import random
 
 def main():
-    # Generar un numero aleatorio entre 1 y 10
+    # Generar un numero secreto entre 1 y 10
     random_number = random.randint(1, 10)
     my_number = int(input("Ingrese numero: "))
 
@@ -15,7 +15,7 @@ def main():
         my_number = int(input("Ingrese numero: "))
 
     print(" ")
-    print(f"Correcto, el numero aleatorio es: {random_number}")
+    print(f"Correcto, el numero secreto es: {random_number}")
 
 
 if __name__ == "__main__":
