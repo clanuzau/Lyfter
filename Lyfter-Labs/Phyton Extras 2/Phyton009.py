841a0d59545b6b30fc8cc68e1681fd6b1d929792
Cesar Lanuza <Cesar.Lanuza@hotmail.com>
2026-07-01 18:02:44 -0600

    feat: update average calculation and list filtering

--- Lyfter-Labs/Phyton Extras 2/Phyton009.py
+++ Lyfter-Labs/Phyton Extras 2/Phyton009.py
@@ -5,20 +5,22 @@ Student: Cesar Lanuza Urbina
 
 def main():
 
-    # Imnport from statistics the function/method mean
-    from statistics import mean
-
     my_values = [10, 20, 30, 35, 40, 45, 50, 60, 65, 70]
-    new_list = [] # This will contain the values higher than average from the list
+    new_list = []  # This will contain values greater than or equal to the average.
 
-    average_number = mean(my_values) # get the average from the list value
+    # Calculate the average value of the list
+    average_number = sum(my_values) / len(my_values)
 
     print()
     print(f"The average number value is: {average_number}")
 
-    for value in my_values[1:]: # read and get the values in the list
+    print()
+    print(f"Original List: {my_values}")
+    print()
+
+    for value in my_values:
         if value >= average_number:
-            new_list.append(value) # If condition is true, add the number to the new list.
+            new_list.append(value)
     
     print(f"New List: {new_list}")
     print()
